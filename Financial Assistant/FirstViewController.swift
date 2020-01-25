cfce486df9a1ca795bc8eb966552c07903dfc64e
Andrii Halabuda <losangelesboy11@gmail.com>
2020-01-25 18:10:21 +0100

    Initial Commit

---
+++ Financial Assistant/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Financial Assistant
+//
+//  Created by Andrew on 1/25/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
